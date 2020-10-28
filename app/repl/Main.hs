e7f23f5f9ea94a2c476f12cc3f70c9eaa5a0e049
JoaoBarreto255 <j.barreto255@gmail.com>
2020-10-28 17:55:48 +0000

    Fix and update repl

--- app/repl/Main.hs
+++ app/repl/Main.hs
@@ -1,19 +1,31 @@
 module Main where
 import Syntax
 import Parser
+import System.IO
 
 main :: IO ()
 main = do 
-     putStrLn "Kaleidoscope Lang Repl"
-     processInput
+    putStrLn "Kaleidoscope Lang Repl\n"
+    processInput
 
 processInput :: IO ()
 processInput = do 
-             putStr ">>> " 
-             line <- getLine
-             putStrLn $ case parseExpr line of
-                Right expr -> show expr
-                Left error -> error
-             processInput
+    line <- promptLine ">>> "
+    if ":exit" /= line then
+        processLine line >> processInput
+    else
+        putStrLn "\n\nBye! :-)"
+
+processLine:: String -> IO ()
+processLine line = putStrLn $ case parseExpr line of
+        Right expr -> formatText $ show expr
+        Left error -> formatText error
+    where formatText str = "\n=>   " ++ str ++ "\n"
+
+promptLine:: String -> IO String
+promptLine header = do
+    putStr header 
+    hFlush stdout
+    getLine
 
         
